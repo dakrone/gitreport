8efba2311cf554c6b348339fd2781570cc856028
Lee Hinman <lee@writequit.org>
2015-04-30 10:17:52 -0600

    Add more formatting for table, add org and markdown link mode

--- src/gitreport/table.clj
+++ src/gitreport/table.clj
@@ -2,5 +2,32 @@
   (:require [doric.core :refer [table org]]
             [gitreport.config :refer [config]]))
 
+(defn format-url-org [url]
+  (let [chunk (first (re-find #"(issues|pull)/\d+" url))]
+    (str "[["
+         "https://github.com/"
+         (config :gh-owner)
+         "/"
+         (config :gh-repo)
+         "/"
+         chunk
+         "]["
+         chunk
+         "]]")))
+
+(defn format-url-markdown [url]
+  (let [chunk (first (re-find #"(issues|pull)/\d+" url))]
+    (str "["
+         chunk
+         "]("
+         "https://github.com/"
+         (config :gh-owner)
+         "/"
+         (config :gh-repo)
+         "/"
+         chunk
+         ")")))
+
 (defn tablify-issues [issues]
-  (table {:format org} (keys (first issues)) issues))
+  (table {:format (config :table-format)}
+         (config :doric-headers) issues))
